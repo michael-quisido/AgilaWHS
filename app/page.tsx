fc8ca5009a05350025932c2d674378e292c14dc3
Mike Quisido <mike082112@gmail.com>
2026-02-19 10:05:43 +0800

    Update page.tsx with location info

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Your Web Hosting Solution. We provide reliable and secure web hosting services to help you establish your online presence with ease. <br /> Agila WHS is funded and owned by KMCQ Company.
+          Your Web Hosting Solution. We provide reliable and secure web hosting services to help you establish your online presence with ease. <br /> Agila WHS is funded and owned by KMCQ Company based in Cebu, Philippines.
         </h2>
       </div>
     </div>
